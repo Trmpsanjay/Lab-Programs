5e9db68beca75fb4e62b1bf8f6ba97e0766f3e88
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2019-11-05 22:53:59 +0530

    Add files via upload

---
+++ hybrid_inheritance.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+
+using namespace std;
+
+
+class student
+{
+	protected:
+		char name[40];
+		int roll_number;
+	public:
+		void get_student_details()
+		{
+			cout<<"Enter Name : "<<endl;
+			cin>>name;
+			cout<<"Enter Roll Number : "<<endl;
+			cin>>roll_number;
+		}
+		
+		void put_students_details()
+		{
+			cout<<" Name : "<<name<<endl;
+			cout<<"Roll number : "<<roll_number<<endl;
+		}
+};
+
+class test: public student
+{
+	protected:
+		float sub1,sub2;
+	public:
+		void get_marks(float x,float y)
+		{
+			sub1=x;sub2=y;
+		}
+		void put_marks()
+		{
+			cout<<"Marks in Subject 1 : "<<sub1<<endl;
+			cout<<"Marks in Subject 2 : "<<sub2<<endl;
+		}
+};
+
+class sports
+{
+	protected:
+		float score;
+	public:
+		void get_score(float s)
+		{
+			score=s;
+		}
+		
+		void put_score()
+		{
+			cout<<"Score Weightage is : "<<score;
+		}
+		
+};
+
+class result: public test,public sports
+{
+	private:
+		float	total;
+	public:
+		void get_result()
+		{
+			total=sub1+sub2+score;
+			cout<<"Total Marks Obtained is : "<<total<<endl;
+		}
+	
+};
+
+
+int main()
+{
+	result r;
+	r.get_student_details();
+	r.get_marks(65.99,34.01);
+	r.get_score(56.0);
+	r.put_students_details();
+	r.put_marks();
+	r.put_score();
+	r.get_result();
+}
